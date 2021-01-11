135e6a7f8db14c756035cea80c2817236e1e6e54
LemonTeaIce <2020971151@isiswa.uitm.edu.my>
2021-01-11 07:06:34 +0000

    commit

--- server.py
+++ server.py
@@ -20,8 +20,8 @@ qty =0
 def process_start(s_sock):
   global data,sum,co
   s_sock.send(str.encode(data))
-  name = s_sock.recv(2048).decode('utf-8')
-  s_sock.send(str.encode(name))
+  name1 = s_sock.recv(2048).decode('utf-8')
+  s_sock.send(str.encode(name1))
   num = s_sock.recv(2048).decode('utf-8')
   s_sock.send(str.encode(num))
 
@@ -47,8 +47,6 @@ def process_start(s_sock):
     
       #insert data to list
       n.append([name,cost,qty,sum])
-      print("List n")
-      print (n)
 
     elif str(opt.decode('ascii')) == '99':
 
@@ -88,60 +86,43 @@ def process_start(s_sock):
                 tot1 = int(qty1)*float(cost1)
                 n[list][3] = tot1
                 if n[list][0] == ans1:
-                  print("list")
-                  print(list)
-                  print("ans1")
-                  print (ans1)
-                  print ("n[list][0]")
-                  print(n[list][0])
                   qty2 = n[list][2]
                   cost2=n[list][1]
                   tot=int(qty2)*float(cost2)
                   print(tot)
-                  y = list-1
-                  if list == 0:
-                    y = list
-                else :
-                  y =0
+                  y = list
+                  y1 = n[y][3]
               tot1 = n[list][3]
               tot2 = tot2+tot1 #total bef delete
+            print(y)
             print("Delete list==>")
-            print(n[y])
-            del n[y][0]
-            del n[y][1]
-            del n[y][2]
-            del n[y][3]
-            print (n[y][3]) #cost delete
-            l = n[y][3]
+            del n[y]
             tot1 = tot1+tot1
-            tots = tot2 - n[y][3]
             global tott1
             tott=0
             for i in range(len(n)):
               for j in range (len(n[i])):
                 print(n[i][3])
                 tott1= n[i][3]
-              tott+=tott+tott1
-              print(tott1)
-            print (tot2)
-            print (tot)
-            print ("tot1b")
-            print (tot1)
-            print (n)
-            
+              tott= tott+tott1
+              print(tott)
+            print (n) #lepas delete
             anss2 = s_sock.recv(2048).decode('utf-8')
             print (anss2)
             s_sock.send(bytes(str(anss2),'ascii'))
             anss3 = s_sock.recv(2048).decode('utf-8')
-            print ("anss3")
+            print (anss3)
             s_sock.send(bytes(str(n),'ascii'))
-            
+            anss4 = s_sock.recv(2048).decode('utf-8')
+            s_sock.send(bytes(str(tott),'ascii'))
 
       #insert list to table
       x = (tabulate(n, headers=['Product Name','Price','Quantity','Amount(RM)'],tablefmt="grid",colalign=("center","center","center","center")))
-
+      y = name1
+      z = num
+      z1 = str(tott)
       #write table into file
-      receipt_file(x)
+      receipt_file(x,y,z,z1)
 
       #send_file
       print("Connection endend")
@@ -155,38 +136,49 @@ def process_start(s_sock):
 
   s_sock.close()
 
-def receipt_file(x):
+def receipt_file(x,y,z,z1):
   date_time = datetime.date.today()
   date_time_str = str(date_time)
   extension = ".txt"
   filename = date_time_str + extension
 
   with open(filename,'w') as f:
-    f.write('Order Information\n\n')
+    f.write("Name: ")
+    f.write(y)
+    f.write("\nNumber phone: ")
+    f.write(z)
+    f.write('\nTotal Price(RM): ')
+    f.write(z1)
+    f.write("\nOrder Information\n\n")
     f.close()
 
   with open(filename, 'a') as f:
     f.write(x)
 
   f.close()
+ 
 
-def delete_detail():
-  ans1 = "yes"
-  ans2 = "Yes"
-  ans = input("Do you want to delete item?")
-  if ans == ans1 or ans2:
-    print ("delete time...")
-    print ("The ori dict..:"+ str(d))
-    inp2 = input("Enter item code :")
-    if inp2 in d:
-      del d[inp2]
-      pop_e = d.pop(inp2)
-      print ("The dict after delete :"+ str(d))
-      print ("Item that has been delete :"+ str(pop_e))
-    else:
-      print ("Option dont exist")
-  else:
-    print ("you dont delete anything")
+def delete_detail(n,y):
+  print ("y")
+  print (y)
+  print ("Nilai n")
+  print ("n[0]")
+  print (n[0])
+  print ("n[1]")
+  print (n[1])
+  print ("Delete n[y]")
+  print (n[y])
+  del n[y]
+  print ("N after")
+  print (n)
+  for i in range(len(n)):
+    tot1 =0
+    print("tot in func")
+    for j in range(len(n[i])):
+      tot= n[i][3]
+      print (n[i][j])
+    tot1= tot1+tot
+    print (tot1)
 
 if __name__ == '__main__':
 
